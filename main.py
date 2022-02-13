1e493a55cf3069b3782129580c65a2b7e1e98f02
User <s.chikita@mail.ru>
2022-02-13 13:47:44 +0300

    emil

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-lst = [int(i) for i in input().split()]
-some_list = []
-
-
-def chetValue(lstOut, lstIn: list) -> list:
-    for i in lstOut:
-        if i % 2 == 0 and i != 0:
-            lstIn.append(i)
-    return lstIn
-
-print(chetValue(lst, some_list))
+a = 5
+b = 10
+print(a + b)
